cc00e4caf6bb497e3101730e58c652a563dc996a
gitgernit <petr.zakharov.anton@gmail.com>
2024-04-14 14:51:40 +0300

    feat: added a separate homepage app

--- chattr/chattr/urls.py
+++ chattr/chattr/urls.py
@@ -1,18 +1,18 @@
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
-import django.views.generic
+import homepage.urls
 
 import api.urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path(
         '',
-        django.views.generic.TemplateView.as_view(template_name='index.html'),
+        include(homepage.urls),
     ),
     path(
         'api/',
         include(api.urls),
     ),
+    path('admin/', admin.site.urls),
 ]
