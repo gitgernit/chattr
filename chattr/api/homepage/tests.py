2f7ba4ee4fc3cd62fd173e0111465f1c407f4597
OgurecRoman <treninasonya@yandex.ru>
2024-04-16 02:19:09 +0300

    add tests to api endpoint

--- chattr/api/homepage/tests.py
+++ chattr/api/homepage/tests.py
@@ -4,18 +4,12 @@ import django.core.exceptions
 import django.test
 from django.test import Client, TestCase
 import django.urls
-import parameterized
 
 
 class ApiEndpointsTest(TestCase):
-    @parameterized.parameterized.expand(
-        [
-            ("1", HTTPStatus.OK),
-        ],
-    )
-    def test_api_endpoint(self, url, expected_status):
-        response = Client().get(django.urls.reverse("api_homepage:get_room"))
-        self.assertEqual(response.status_code, expected_status)
+    def test_api_endpoint(self):
+        response = django.test.Client().get(f"/api/homepage/get_room/")
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
 
 __all__ = []
--- chattr/api/homepage/urls.py
+++ chattr/api/homepage/urls.py
@@ -7,7 +7,6 @@ app_name = 'api_homepage'
 urlpatterns = [
     rest_framework.urls.path(
         'get_room/',
-        api.homepage.views.NewRoom.as_view(),
-        name='get_room',
+        api.homepage.views.NewRoom.as_view()
     ),
 ]
