ad8ff613f990d2359fec94fdefa7878a55e83d2f
ZolotarevAlexandr <Zolotarev10032006@mail.ru>
2024-04-16 18:14:32 +0300

    fix: added minimum values for homepage validators

--- chattr/api/homepage/validators.py
+++ chattr/api/homepage/validators.py
@@ -8,7 +8,11 @@ class MaxUsersValidator:
         self.max_users = max_users
 
     def __call__(self, value):
-        if value > self.max_users:
+        if value <= 1:
+            raise rest_framework.exceptions.ValidationError(
+                'Max users must be greater than 1',
+            )
+        if value >= self.max_users:
             raise rest_framework.exceptions.ValidationError(
                 f'Max users must be less than or equal to {self.max_users}',
             )
@@ -19,7 +23,11 @@ class MaxIdleTimeValidator:
         self.max_idle_time = max_idle_time
 
     def __call__(self, value):
-        if value > self.max_idle_time:
+        if value <= 0:
+            raise rest_framework.exceptions.ValidationError(
+                'Max idle time must be greater than 0',
+            )
+        if value >= self.max_idle_time:
             raise rest_framework.exceptions.ValidationError(
                 f'Max idle time must be less than or '
                 f'equal to {self.max_idle_time}',
