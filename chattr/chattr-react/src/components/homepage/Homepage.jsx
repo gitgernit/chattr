38ed273dba5e43710d1b2fa5948ee26396d5bbfd
gitgernit <petr.zakharov.anton@gmail.com>
2024-04-19 15:50:36 +0300

    improvement: base homepage, toastify

---
+++ chattr/chattr-react/src/components/homepage/Homepage.jsx
@@ -0,0 +1,51 @@
+import './Homepage.css';
+import {useState} from 'react';
+import ReactModal from "react-modal";
+import {ToastContainer, toast, Flip} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+
+const copyLink = async () => {
+  const textbox = document.getElementById('link-textbox');
+  const theme = localStorage.getItem('theme')
+
+  try {
+    await navigator.clipboard.writeText(textbox.value);
+    toast.success('Link saved to clipboard', {
+      position: "top-left",
+      autoClose: 2000,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      transition: Flip,
+      theme: theme,
+    });
+  } catch (error) {
+    console.log('shit')
+  }
+}
+
+
+ReactModal.setAppElement('#root')
+
+function Homepage() {
+  const [modalIsOpen, setModalOpen] = useState(false)
+
+  return (
+    <>
+      <div className="homepage">
+        <div className="link-generator">
+          <input type="text" id="link-textbox"
+                 onClick={copyLink} readOnly/>
+          <button id="link-settings">
+            <img src="src/assets/cogs.svg" alt="Modal with room settings"
+                 id="settings-img"/>
+          </button>
+          <ReactModal/>
+        </div>
+        <ToastContainer/>
+      </div>
+    </>
+  )
+}
+
+export default Homepage
