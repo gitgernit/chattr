4c05e4409dfe160bacc1b6856c2043792e19c58c
ZolotarevAlexandr <Zolotarev10032006@mail.ru>
2024-04-24 00:15:54 +0300

    feature: added basic models for user and message in core app

---
+++ chattr/core/models.py
@@ -0,0 +1,34 @@
+import django.contrib.auth.models
+import django.db.models
+
+
+class ChattrUser(django.contrib.auth.models.AbstractUser):
+    identifier = django.db.models.CharField(
+        name='identifier',
+        verbose_name='UUID4 identifier',
+        max_length=255,
+        unique=True,
+        db_index=True,
+    )
+    username = django.db.models.CharField(
+        name='username',
+        verbose_name='username',
+        max_length=255,
+    )
+
+
+class Message(django.db.models.Model):
+    sender = django.db.models.ForeignKey(
+        ChattrUser,
+        name='sender',
+        on_delete=django.db.models.CASCADE,
+        related_name='sent_messages',
+    )
+    ws_group = django.db.models.CharField(
+        name='ws_group',
+        verbose_name='websocket group',
+    )
+    content = django.db.models.TextField(
+        name='content',
+        verbose_name='content',
+    )
