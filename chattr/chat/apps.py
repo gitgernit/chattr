ec22945a2525895576a53ddd93189ef868223131
gitgernit <petr.zakharov.anton@gmail.com>
2024-04-24 22:51:40 +0300

    improvement: better app configs

--- chattr/chat/apps.py
+++ chattr/chat/apps.py
@@ -6,3 +6,5 @@ from django.apps import AppConfig
 class ChatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'chat'
+    label = 'chat'
+    verbose_name = 'Вебсокет-чат'
