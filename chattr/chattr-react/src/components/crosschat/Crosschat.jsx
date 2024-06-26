d64564c9e6ad85bf62ae52d9fe1e0c7f8fb4d1a9
gitgernit <petr.zakharov.anton@gmail.com>
2024-06-27 02:57:14 +0300

    improvement: base styled .controls layout

--- chattr/chattr-react/src/components/crosschat/Crosschat.jsx
+++ chattr/chattr-react/src/components/crosschat/Crosschat.jsx
@@ -4,7 +4,8 @@ function Crosschat() {
   return (
     <div className="crosschat">
       <div className="controls">
-        <textarea id="123"/>
+        <textarea className="sdp-area" id="sdp-area"
+                  placeholder="Paste \ copy SDP"/>
         <div className="sdp-buttons">
           <button>Offer</button>
           <button>Answer</button>
