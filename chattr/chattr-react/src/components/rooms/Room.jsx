73d4297c34bea6b0e7393c2e439a34c7356e7bbe
gitgernit <petr.zakharov.anton@gmail.com>
2024-07-10 18:32:26 +0300

    🐛feat(rooms): fixed video frame behaviour
    fixed video frame resizing upon overflowing due to being non-absolutionaly positioned, now using an
    absolutely positioned wrapper

--- chattr/chattr-react/src/components/rooms/Room.jsx
+++ chattr/chattr-react/src/components/rooms/Room.jsx
@@ -1,13 +1,17 @@
 import './Room.css'
 
 function Room() {
-    return (
-      <div className="room">
-        <div className="videos">
+  return (
+    <div className="room">
+      <div className="videos">
+        <div className="videos-wrapper">
           <video/>
         </div>
       </div>
-    )
+      <div className="chat">
+      </div>
+    </div>
+  )
 }
 
 export default Room
