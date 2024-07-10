bdf14aa8840acaee46b7ab5bae4e609d4f2b3412
gitgernit <petr.zakharov.anton@gmail.com>
2024-07-10 03:12:48 +0300

    ✨💄feat(rooms): styled, refubrished and finally working inline videos frame
    the inline videos frame is now styled alike with the rest of the project, works like a grid, allows
    scrolling, overflowing, and all other kinds of things

--- chattr/chattr-react/src/components/rooms/Room.jsx
+++ chattr/chattr-react/src/components/rooms/Room.jsx
@@ -3,17 +3,8 @@ import './Room.css'
 function Room() {
     return (
       <div className="room">
-        <div className="inline-divs">
-          <div className="filler"/>
-          <div className="filler"/>
-          <div className="filler"/>
-          <div className="filler"/>
-          <div className="filler"/>
-          <div className="filler"/>
-          <div className="filler"/>
-          <div className="filler"/>
-          <div className="filler"/>
-          <div className="filler"/>
+        <div className="videos">
+          <video/>
         </div>
       </div>
     )
