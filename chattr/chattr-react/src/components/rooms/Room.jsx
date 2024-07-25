54509a4137c2b51d5ae9d9209fe96a676fb3c3bf
gitgernit <petr.zakharov.anton@gmail.com>
2024-07-25 05:42:53 +0300

    :sparkles:feat(rooms): initial user on page load
    created a User class - this one represents the user and currently only stores the username but will
    probably store user's media and audio tracks aswell

--- chattr/chattr-react/src/components/rooms/Room.jsx
+++ chattr/chattr-react/src/components/rooms/Room.jsx
@@ -1,34 +1,28 @@
-import './Room.css'
+import { useState, useEffect } from 'react';
+import { User } from './User.js';
+import './Room.css';
 
 function Room() {
+  const [users, setUsers] = useState([new User('you')]);
+
   return (
     <div className="room">
       <div className="videos">
         <div className="videos-wrapper">
-          <div className="user-video">
-            <video/>
-            <span className="video-username">
-              undefined
-            </span>
-          </div>
-          <div className="user-video">
-            <video/>
-            <span className="video-username">
-              undefined
-            </span>
-          </div>
-          <div className="user-video">
-            <video/>
-            <span className="video-username">
-              undefined
-            </span>
-          </div>
+          {users.map((user) => (
+            <div key={user.name} className="user-video">
+              <video />
+              <span className="video-username">
+                {user.name}
+              </span>
+            </div>
+          ))}
         </div>
       </div>
       <div className="chat">
       </div>
     </div>
-  )
+  );
 }
 
-export default Room
+export default Room;
